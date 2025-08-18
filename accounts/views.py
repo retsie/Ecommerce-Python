11291ecd5da71bf6f35784e1ad0314f09873d573
chie <chiecruzcayanan@gmail.com>
2025-08-18 23:47:48 +0800

    registration and activation

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
-
+from django.contrib import messages, auth
+from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
 from accounts.forms import RegisterForm
-from accounts.models import Account
+from .models import Account
+
+#verification email
+from django.contrib.sites.shortcuts import get_current_site
+from django.template.loader import render_to_string
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+from django.utils.encoding import force_bytes
+from django.contrib.auth.tokens import default_token_generator
+from django.core.mail import EmailMessage
+
+# from django.core.mail import send_mail, EmailMessage
+# from django.utils.encoding import force_bytes
+# from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+# from django.contrib.auth.tokens import default_token_generator
+
 
 
 def register(request):
@@ -17,6 +33,22 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name,last_name=last_name,email=email, username=username, password=password)
             user.phone_number = phone_number
             user.save()
+
+            #user activation
+            current_site = get_current_site(request)
+            mail_subject = 'Activate your account.'
+            message_html = render_to_string('accounts/account_verfication_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid' : urlsafe_base64_encode(force_bytes(user.pk)),
+                'token' : default_token_generator.make_token(user),
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message_html, to=[to_email])
+            send_email.send()
+
+            # messages.success(request, 'Thank you for registering. We have sent you a confirmation link.')
+            return redirect('/accounts/login/?command=verification&email=' + email)
     else:
         form = RegisterForm()
 
@@ -26,7 +58,47 @@ def register(request):
     return render(request, 'accounts/register.html', context)
 
 def login(request):
+    if request.method == 'POST':
+        email = request.POST['email']
+        password = request.POST['password']
+        user = auth.authenticate(email=email, password=password)
+        if user is not None:
+            auth.login(request, user)
+            messages.success(request, 'You are logged in')
+            return redirect('dashboard')
+        else:
+            messages.error(request, 'Email or password is incorrect')
+            return redirect('login')
+
     return render(request, 'accounts/login.html')
 
+@login_required(login_url='login')
 def logout(request):
-    return
+    auth.logout(request)
+    messages.success(request, 'You have been logged out')
+    return redirect('login')
+
+def activate(request, uidb64, token):
+    try:
+        uid = urlsafe_base64_decode(uidb64).decode()
+        user = Account._default_manager.get(pk=uid)
+    except (TypeError, ValueError, OverflowError, Account.DoesNotExist):
+        user = None
+
+    if user is not None and default_token_generator.check_token(user, token):
+        user.is_active = True
+        user.save()
+        messages.success(request, 'Account has been activated')
+        return redirect('login')
+    else:
+        messages.error(request, 'Activation link is invalid')
+        return redirect('register')
+
+
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'accounts/dashboard.html')
+
+
+
+
--- chiekart/settings.py
+++ chiekart/settings.py
@@ -43,6 +43,7 @@ INSTALLED_APPS = [
     'carts',
 ]
 
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
@@ -62,6 +63,7 @@ TEMPLATES = [
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -146,6 +148,24 @@ STATICFILES_DIRS = [
 # media files confirguration
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+
+from django.contrib.messages import constants as messages
+MESSAGE_TAGS = {
+    messages.DEBUG: 'alert-info',
+    messages.INFO: 'alert-info',
+    messages.SUCCESS: 'alert-success',
+    messages.WARNING: 'alert-warning',
+    messages.ERROR: 'alert-danger',
+}
+
+EMAIL_USE_TLS = True
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'e1st3r77@gmail.com'
+EMAIL_HOST_PASSWORD = 'password_here'
+
+
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
